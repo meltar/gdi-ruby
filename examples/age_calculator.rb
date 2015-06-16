de0a0bc279a3dc2ff3fc7479ccd49677e03488c4
Melissa Holmes <meltar@gmail.com>
2015-06-15 22:16:06 -0400

    Format code

--- examples/age_calculator.rb
+++ examples/age_calculator.rb
@@ -1,9 +1,11 @@
 puts 'What year were you born?'
 
 year = gets.chomp!.to_i
+
 years_old = 2015 - year
 days_old = years_old * 365
 seconds_old = days_old * 24 * 60 * 60
+
 puts 'You are ' + years_old.to_s + ' years old.'
 puts 'You are ' + days_old.to_s + ' days old.'
 puts 'You are ' + seconds_old.to_s + ' seconds old.'
